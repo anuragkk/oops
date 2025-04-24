84e86b1c8f532fcdaa2bfcd0e753345ecdd1f025
=Anurag <=anuragsnd@gmail.com>
2025-04-24 21:49:16 +0530

    practice

---
+++ practice.py
@@ -0,0 +1,24 @@
+class User:
+    def __init__(self, user_id, user_name, followers=0, following=0):
+        self.user_id = user_id
+        self.user_name = user_name
+        self.followers = followers
+        self.following = following
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+
+user_1 = User("001", "Anurag")
+user_2=User("002", "Golu")
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
+
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
